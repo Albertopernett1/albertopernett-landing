fc43681dfe2d30d0d5271038805579bc77b6ba49
Alberto <albertopernett94@gmail.com>
2025-04-08 12:22:14 -0500

    Add custom favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <link rel="icon" href="/editado.PNG" />
+        <title>Alberto Pernett | Freelancer</title>
+      </Head>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   )
 }
