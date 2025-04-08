cac9ce6dabdfcbe524643e0407ba9c07fe51ca7f
Alberto <albertopernett94@gmail.com>
2025-04-08 13:24:28 -0500

    Integrate Google Analytics 4 with ID G-7GHFRW5D22

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
+import Script from 'next/script'
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ['latin'] })
@@ -13,6 +14,25 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/editado.PNG" />
         <title>Alberto Pernett | Freelancer</title>
       </Head>
+      
+      {/* Google Analytics 4 */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-7GHFRW5D22`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7GHFRW5D22');
+          `,
+        }}
+      />
+      
       <main className={inter.className}>
         <Component {...pageProps} />
       </main>
